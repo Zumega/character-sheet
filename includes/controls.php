8d05453543f127b35fc99c5163674a84df88ca10
Ross McAdams <rpm1977@gmail.com>
2012-10-05 11:10:32 -0700

    pulled used points for skills from DB

--- includes/controls.php
+++ includes/controls.php
@@ -1,8 +1,14 @@
 <?php
 require_once 'connection_Open.php';
-$query = "SELECT  intStrength + intAgility + intVitality + intAlertness + intIntelligence + intWillpower usedPoints FROM sheet_attributes WHERE id = ". $_SESSION['id'];
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
-$line = mysql_fetch_array($result, MYSQL_ASSOC);
+$queryAttr = "SELECT  intStrength + intAgility + intVitality + intAlertness + intIntelligence + intWillpower usedPoints FROM sheet_attributes WHERE id = ". $_SESSION['id'];
+$resultAttr = mysql_query($queryAttr) or die('Query failed: ' . mysql_error());
+$lineAttr = mysql_fetch_array($resultAttr, MYSQL_ASSOC);
+$usedAttrPoints = $lineAttr['usedPoints'];
+
+$querySkill = "SELECT SUM(intSubSkillDice_2 +  intSubSkillDice_1 +  intSubSkillDice_0 +  intSkillDice) AS usedPoints FROM  sheet_user_skills WHERE  id = ". $_SESSION['id'];
+$resultSkill = mysql_query($querySkill) or die('Query failed: ' . mysql_error());
+$lineSkill = mysql_fetch_array($resultSkill, MYSQL_ASSOC);
+$usedSkillPoints = $lineSkill['usedPoints'];
 require_once 'connection_Close.php';
 
 ?>
@@ -13,14 +19,14 @@ require_once 'connection_Close.php';
       <label for="attrPoints">Attribute Points:</label>
       <ul>
         <li>Max: <input type="text" value="<?= $attrPoints ?>" id="attrPoints" name="attrPoints" readonly="readonly" class="noEdit" tabindex="-1" />
-        <li>Used: <input type="text" value="<?= $line['usedPoints'] ?>" id="usedAttrPoints" name="usedAttrPoints" readonly="readonly" class="noEdit" tabindex="-1" />
+        <li>Used: <input type="text" value="<?= $usedAttrPoints ?>" id="usedAttrPoints" name="usedAttrPoints" readonly="readonly" class="noEdit" tabindex="-1" />
       </ul>
     </li>
     <li>
       <label for="skillPoints">Skill Points:</label>
       <ul>
         <li>Max: <input type="text" value="<?= $skillPoints ?>" id="skillPoints" name="skillPoints" readonly="readonly" class="noEdit" tabindex="-1" /></li>
-        <li>Used: <input type="text" value="0" id="usedSkillPoints" name="usedSkillPoints" readonly="readonly" class="noEdit" tabindex="-1" /></li>
+        <li>Used: <input type="text" value="<?= $usedSkillPoints; ?>" id="usedSkillPoints" name="usedSkillPoints" readonly="readonly" class="noEdit" tabindex="-1" /></li>
       </ul>
     </li>
 <!--    <li><a href="javascript:SaveArea();">Save</a></li>
