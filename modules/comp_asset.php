328bb1025dea280b67e2a8590e746973f307ac23
Ross McAdams <rpm1977@gmail.com>
2012-11-05 08:50:09 -0800

    updated some UI, moved up/down buttons for skills/comp/asset

--- modules/comp_asset.php
+++ modules/comp_asset.php
@@ -9,8 +9,10 @@ $amount = $sqlInfo->num_rows();
 <div class="module comp_asset <?= $comp_asset ?> clear">
   <div>
     You have <span id="<?= $comp_asset ?>Cnt"><?= $amount; ?></span> <?= $comp_asset; ?>
-    <input type="button" id="<?= $comp_asset ?>Up" class="btnUpDown btnUp" tabindex="-1" />
-    <input type="button" id="<?= $comp_asset ?>Down" class="btnUpDown btnDown<?= ($amount > 0) ? '' : ' hide' ?>" tabindex="-1" />
+    <div class="compAssetButtons">
+      <input type="button" id="<?= $comp_asset ?>Up" class="btnUpDown btnUp" tabindex="-1" />
+      <input type="button" id="<?= $comp_asset ?>Down" class="btnUpDown btnDown<?= ($amount > 0) ? '' : ' hide' ?>" tabindex="-1" />
+    </div>
   </div>
   <div id="<?= $comp_asset ?>Fields" class="">
     <?php
