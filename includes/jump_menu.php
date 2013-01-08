2a4815d09d42392a491fa8b287834d2124fa573d
Ross McAdams <rpm1977@gmail.com>
2013-01-08 15:23:31 -0800

    updated the jump menu and added some "flare" to the doc when the menu is used.

--- includes/jump_menu.php
+++ includes/jump_menu.php
@@ -1,14 +1,31 @@
+<?php
+function _jumpMenu() {
+  $areas = array(
+    'top' => 'Top',
+    'characterInfo' => 'Character Info',
+    'attributes' => 'Attributes',
+    'derivedTraits' => 'Derived Traits',
+    'skillsSpecialties' => 'Skills and Specialties',
+    'equipment' => 'Equipment',
+    'complications' => 'Complications',
+    'assets' => 'Assets',
+    'info' => 'Info',
+    'dice' => 'Dice',
+    'bottom' => 'Bottom'
+  );
+?>
+
 <div class="jumpMenu">
   <ul>
-    <li><a href="#top" class="smallLink" tabindex="-1">Top</a></li>
-    <li><a href="#attributes" class="smallLink" tabindex="-1">Attributes</a></li>
-    <li><a href="#derived_traits" class="smallLink" tabindex="-1">Derived Traits</a></li>
-    <li><a href="#skills_and_specialties" class="smallLink" tabindex="-1">Skills and Specialties</a></li>
-    <li><a href="#equipment" class="smallLink" tabindex="-1">Equipment</a></li>
-    <li><a href="#complications" class="smallLink" tabindex="-1">Complications</a></li>
-    <li><a href="#assets" class="smallLink" tabindex="-1">Assets</a></li>
-    <li><a href="#info" class="smallLink" tabindex="-1">Info</a></li>
-    <li><a href="#dice" class="smallLink" tabindex="-1">Dice</a></li>
-    <li><a href="#bottom" class="smallLink end" tabindex="-1">Bottom</a></li>
+    <?php
+      foreach($areas as $link => $text) {
+        ?>
+        <li><a href="#<?= $link; ?>" class="smallLink" tabindex="-1"><?= $text; ?></a></li>
+    <?php
+      }
+    ?>
   </ul>
 </div>
+<?php
+}
+?>
